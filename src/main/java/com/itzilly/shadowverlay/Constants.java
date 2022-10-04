beabfe3124705edad1b537a565c34b8d2114c9a1
illyum <90023277+itzilly@users.noreply.github.com>
2022-10-04 17:09:44 -0600

    Add message prefixes

--- src/main/java/com/itzilly/shadowverlay/Constants.java
+++ src/main/java/com/itzilly/shadowverlay/Constants.java
@@ -4,4 +4,8 @@ package com.itzilly.shadowverlay;
 public class Constants {
     public static final String LOG_LOCATION = "";
     public static String API_KEY = null;
+    public static String LIST_MESSAGE_PREFIX = "[Client thread/INFO]: [CHAT] ONLINE: ";
+    public static String PLAYER_QUERY_PREFIX = "[Client thread/INFO]: [CHAT] Can't find a player by the name of '.";
+    public static String NEW_KEY_PREFIX = "[Client thread/INFO]: [CHAT] Your new api key is: ";
+
 }
