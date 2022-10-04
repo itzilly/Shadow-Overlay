e1a329555a2b5b641e8caa5c4e2d2fef93eb59cd
illyum <90023277+itzilly@users.noreply.github.com>
2022-10-04 17:05:17 -0600

    Add constants
    Added API Key
    Added Log File Path

--- src/main/java/com/itzilly/shadowverlay/Constants.java
+++ src/main/java/com/itzilly/shadowverlay/Constants.java
@@ -1,2 +1,7 @@
-package com.itzilly.shadowverlay;public class Constants {
+package com.itzilly.shadowverlay;
+
+
+public class Constants {
+    public static final String LOG_LOCATION = "";
+    public static String API_KEY = null;
 }
