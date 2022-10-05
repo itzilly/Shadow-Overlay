eb8c1d8904779ef78ef46b88740719422ee5f5c3
illyum <90023277+itzilly@users.noreply.github.com>
2022-10-04 18:15:07 -0600

    Add constants

--- src/main/java/com/itzilly/shadowverlay/Constants.java
+++ src/main/java/com/itzilly/shadowverlay/Constants.java
@@ -2,10 +2,13 @@ package com.itzilly.shadowverlay;
 
 
 public class Constants {
-    public static final String LOG_LOCATION = "";
+    public static String LOG_LOCATION = null;
     public static String API_KEY = null;
     public static String LIST_MESSAGE_PREFIX = "[Client thread/INFO]: [CHAT] ONLINE: ";
     public static String PLAYER_QUERY_PREFIX = "[Client thread/INFO]: [CHAT] Can't find a player by the name of '.";
     public static String NEW_KEY_PREFIX = "[Client thread/INFO]: [CHAT] Your new api key is: ";
+
+    public static LogTailer logTailer = null;
+    public static Thread logTailerThread = null;
 
 }
