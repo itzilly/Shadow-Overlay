e394a31fe29bfc697be2bd3a6b6b0c4e7e2010c9
illyum <90023277+itzilly@users.noreply.github.com>
2022-10-05 01:19:43 -0600

    Add getters/setters for stars variable

--- src/main/java/com/itzilly/shadowOverlay/objects/GamePlayer.java
+++ src/main/java/com/itzilly/shadowOverlay/objects/GamePlayer.java
@@ -2,9 +2,11 @@ package com.itzilly.shadowOverlay.objects;
 
 public class GamePlayer {
     private String name;
+    private int stars;
 
     public GamePlayer(String name) {
         this.name = name;
+        this.stars = 0;
     }
 
     public String getName() {
@@ -13,5 +15,13 @@ public class GamePlayer {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public int getStars() {
+        return stars;
+    }
+
+    public void setStars(int stars) {
+        this.stars = stars;
     }
 }
