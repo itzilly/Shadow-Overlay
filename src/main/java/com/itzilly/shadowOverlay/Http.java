17ea668c1ff937d86458531045733680964bed4a
illyum <90023277+itzilly@users.noreply.github.com>
2022-10-06 02:57:01 -0600

    Add key tester functionality

--- src/main/java/com/itzilly/shadowOverlay/Http.java
+++ src/main/java/com/itzilly/shadowOverlay/Http.java
@@ -1,4 +1,23 @@
 package com.itzilly.shadowOverlay;
 
+import me.kbrewster.exceptions.APIException;
+import me.kbrewster.hypixelapi.HypixelAPI;
+
+import java.io.IOException;
+
 public class Http {
+    public static boolean isValidKey(String key) {
+        HypixelAPI hypixelAPI = new HypixelAPI(key);
+        try {
+            hypixelAPI.getKeyInfo();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } catch (APIException e) {
+            if (e.getMessage().equals("Invalid API key")) {
+                return false;
+            }
+            throw new RuntimeException(e);
+        }
+        return true;
+    }
 }
