7169f5d38e072b3dcde3d3a4bd104af9512fb71f
illyum <90023277+itzilly@users.noreply.github.com>
2022-10-06 02:56:51 -0600

    Fix api prefix typo

--- src/main/java/com/itzilly/shadowOverlay/Constants.java
+++ src/main/java/com/itzilly/shadowOverlay/Constants.java
@@ -2,8 +2,8 @@ package com.itzilly.shadowOverlay;
 
 public class Constants {
 
-    public static String BUILD_NUMBER = "b4";
-    public static String VERSION = "A0.2.1";
+    public static String BUILD_NUMBER = "b5";
+    public static String VERSION = "A0.2.2";
     public static String VERSION_SHORTHAND = VERSION + "-" + BUILD_NUMBER;
     public static String WINDOW_TITLE = "Shadow Overlay " + VERSION_SHORTHAND;
 
@@ -12,7 +12,7 @@ public class Constants {
 
     public static String LIST_MESSAGE_PREFIX = "[Client thread/INFO]: [CHAT] ONLINE: ";
     public static String PLAYER_QUERY_PREFIX = "[Client thread/INFO]: [CHAT] Can't find a player by the name of '.";
-    public static String NEW_KEY_PREFIX = "[Client thread/INFO]: [CHAT] Your new api key is: ";
+    public static String NEW_KEY_PREFIX = "[Client thread/INFO]: [CHAT] Your new API key is ";
 
     public static LogTailer LOG_TAILER = null;
     public static Thread LOG_TAILER_THREAD = null;
