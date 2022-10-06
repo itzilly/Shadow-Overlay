049a75320d7abd096f7b470f37f3050ed14eac0d
illyum <90023277+itzilly@users.noreply.github.com>
2022-10-06 03:02:12 -0600

    Add feature to grab stars from playerdata

--- src/main/java/com/itzilly/shadowOverlay/objects/GamePlayer.java
+++ src/main/java/com/itzilly/shadowOverlay/objects/GamePlayer.java
@@ -1,12 +1,25 @@
 package com.itzilly.shadowOverlay.objects;
 
+import com.itzilly.shadowOverlay.Constants;
+import me.kbrewster.exceptions.APIException;
+import me.kbrewster.hypixelapi.HypixelAPI;
+import me.kbrewster.hypixelapi.player.HypixelPlayer;
+
+import java.io.IOException;
+
 public class GamePlayer {
     private String name;
     private int stars;
 
     public GamePlayer(String name) {
         this.name = name;
-        this.stars = 0;
+        HypixelAPI hypixelAPI = new HypixelAPI(Constants.API_KEY);
+        try {
+            HypixelPlayer hypixelPlayer = hypixelAPI.getPlayer(name);
+            this.stars = (int) hypixelPlayer.getAchievements().getBedwarsLevel();
+        } catch (APIException | IOException e) {
+            this.stars = -1;
+        }
     }
 
     public String getName() {
