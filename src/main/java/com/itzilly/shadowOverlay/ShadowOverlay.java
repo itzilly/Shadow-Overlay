c2ed83f221499f85c61d7ebf743375d71c061af7
illyum <90023277+itzilly@users.noreply.github.com>
2022-10-09 22:43:33 -0600

    Initialize main config

--- src/main/java/com/itzilly/shadowOverlay/ShadowOverlay.java
+++ src/main/java/com/itzilly/shadowOverlay/ShadowOverlay.java
@@ -4,7 +4,7 @@ import com.itzilly.shadowOverlay.ui.MainWindow;
 
 public class ShadowOverlay {
     public static void main(String[] args) {
-        System.out.println("Starting Shadow Overlay");
+        Constants.SETTINGS_MANAGER = new SettingsManager();
         MainWindow.showOverlay();
     }
 }
