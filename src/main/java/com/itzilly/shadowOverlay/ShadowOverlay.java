6ebe8d963ed0f244c6bfd31d073f1f3a7b43e414
illyum <90023277+itzilly@users.noreply.github.com>
2022-10-11 05:04:21 -0600

    Add setOut and setErr for debugging purposes

--- src/main/java/com/itzilly/shadowOverlay/ShadowOverlay.java
+++ src/main/java/com/itzilly/shadowOverlay/ShadowOverlay.java
@@ -2,8 +2,14 @@ package com.itzilly.shadowOverlay;
 
 import com.itzilly.shadowOverlay.ui.MainWindow;
 
+import java.io.FileOutputStream;
+import java.io.PrintStream;
+
 public class ShadowOverlay {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
+        System.setOut(new PrintStream(new FileOutputStream("latest.out")));
+        System.setErr(new PrintStream(new FileOutputStream("latest.err")));
+
         // Generate default config file
         new YmlConfig();
  
