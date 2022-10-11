6c9b3a50a5d899b2219cbaadb756a47e0ea680c9
illyum <90023277+itzilly@users.noreply.github.com>
2022-10-10 20:52:59 -0600

    Generate config file

--- src/main/java/com/itzilly/shadowOverlay/ShadowOverlay.java
+++ src/main/java/com/itzilly/shadowOverlay/ShadowOverlay.java
@@ -3,15 +3,11 @@ package com.itzilly.shadowOverlay;
 import com.itzilly.shadowOverlay.ui.MainWindow;
 
 public class ShadowOverlay {
-    static YmlConfig ymlConfig = new YmlConfig();
     public static void main(String[] args) {
-
-        // Change api key
-        ymlConfig.set("API_KEY", "123456");
-
-        // Get information from config
-        System.out.println(ymlConfig.getString("API_KEY"));
-
+        // Generate default config file
+        new YmlConfig();
+ 
+        // Show main window
         MainWindow.showOverlay();
     }
 }
