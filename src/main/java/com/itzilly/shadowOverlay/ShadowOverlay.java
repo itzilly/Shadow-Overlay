10a143e613280ba158872387d738102ecb533200
illyum <90023277+itzilly@users.noreply.github.com>
2022-10-10 20:34:28 -0600

    Remove settings manager

--- src/main/java/com/itzilly/shadowOverlay/ShadowOverlay.java
+++ src/main/java/com/itzilly/shadowOverlay/ShadowOverlay.java
@@ -3,8 +3,15 @@ package com.itzilly.shadowOverlay;
 import com.itzilly.shadowOverlay.ui.MainWindow;
 
 public class ShadowOverlay {
+    static YmlConfig ymlConfig = new YmlConfig();
     public static void main(String[] args) {
-        Constants.SETTINGS_MANAGER = new SettingsManager();
+
+        // Change api key
+        ymlConfig.set("API_KEY", "123456");
+
+        // Get information from config
+        System.out.println(ymlConfig.getString("API_KEY"));
+
         MainWindow.showOverlay();
     }
 }
