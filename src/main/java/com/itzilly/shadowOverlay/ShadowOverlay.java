d84f8caf9745f49224a4cc701e5e2b03d1ad52e7
illyum <90023277+itzilly@users.noreply.github.com>
2022-10-23 10:26:47 -0600

    Generate default config

--- src/main/java/com/itzilly/shadowOverlay/ShadowOverlay.java
+++ src/main/java/com/itzilly/shadowOverlay/ShadowOverlay.java
@@ -1,8 +1,11 @@
 package com.itzilly.shadowOverlay;
 
 import com.itzilly.shadowOverlay.ui.MainWindow;
+import org.ini4j.Ini;
 
+import java.io.File;
 import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.PrintStream;
 
 public class ShadowOverlay {
@@ -10,10 +13,32 @@ public class ShadowOverlay {
         System.setOut(new PrintStream(new FileOutputStream("latest.out")));
         System.setErr(new PrintStream(new FileOutputStream("latest.err")));
 
-        // Generate default config file
-        new YmlConfig();
- 
+        genConfig();
+
         // Show main window
         MainWindow.showOverlay();
+    }
+
+    private static void genConfig() {
+        File configDir = new File("config/");
+        if (!configDir.exists()) {
+            boolean wasMade = configDir.mkdir();
+        }
+
+        File configFile = new File("config/config.properties");
+        if (configFile.exists()) {
+            return;
+        }
+
+
+        Ini writeIni = new Ini();
+        writeIni.put("GENERAL", "API_KEY", "");
+        writeIni.put("GENERAL", "LOG_PATH", "");
+        try {
+            writeIni.store(new FileOutputStream("config/config.properties"));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
     }
 }
